7b73590a9924e1d6128e905eca8c3fb912fbb8a1
Matheus <matheusfavoreto2022@gmail.com>
2024-09-17 12:09:50 -0300

    login main e register code cadastro

---
+++ views/register.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastrar-se</title>
+</head>
+<body>
+   <div>
+      <h2>Cadastro de usuário</h2>
+       <form action="" method="post">
+           <label for="nome">Nome</label>
+           <input type="text" name="nome" id="nome" require> 
+
+           <label for="email">Email</label>
+           <input type="email" name="email" id="email" require>
+
+           <label for="senha">Senha</label>
+           <input type="senha" name="senha" id="senha" require>
+           
+           <label for="perfil">Perfil</label>
+           <select name="perfil" id="perfil" require>
+            <option value="admin">Admin</option>
+            <option value="gestor">Gestor</option>
+            <option value="colaborador">Colaborador</option>
+           </select>
+
+           <button type="submit">Cadastrar</button>
+       </form>
+       <a href=""> Voltsr ao Login</a>  
+   </div> 
+</body>
+</html>
