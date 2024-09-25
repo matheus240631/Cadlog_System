0e29f0c2fdda717a9bc9faeb1250c253a76e0093
Matheus <matheusfavoreto2022@gmail.com>
2024-09-25 09:47:23 -0300

    database dados

--- models/database.php
+++ models/database.php
@@ -2,7 +2,7 @@
 
 class Database{
     //Utilizar padrão Singleton, cujo objtivo é farantir que apenas uma única instância de classe seja criada durante a execução do programa, e que essa instância seja utilizada sempre que necessário 
-   private static $instance = null
+   private static $instance = null;
 
     //Método público que retorna a conexão BD
    public static function getConnection(){
@@ -17,7 +17,7 @@ class Database{
      // A conexâo usa o driver Mysql (mysql:) e as infotmaçôes de host e DB
      self::$instance = new PDO("mysql:host=$host;dbname=$db", $user, $password);
         //define o modo de erro para exceçôes, facilitando a depuração e tratamento do erro
-       self::$instance-->setAttribute(PDO::ATTR_AUTOCOMMIT, PDO::ERRMODE_EXCEPTION);
+       self::$instance->setAttribute(PDO::ATTR_AUTOCOMMIT, PDO::ERRMODE_EXCEPTION);
          
     }
       return self::$instance;
