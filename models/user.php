da28a7849a8bb2d7ec6b5cca612c7ce948b9f714
Matheus <matheusfavoreto2022@gmail.com>
2024-09-30 08:10:42 -0300

    code  melhor user para execute email

--- models/user.php
+++ models/user.php
@@ -3,9 +3,26 @@
 
  class User
 {
+   // Função para localizar usuário pelo email 
  public static function findByEmail($email){
-    
- }
+    $conn = Database::getConnection();
+    $stmt = $conn->prepare("SELECT * FROM usarios WHERE email  = :email");
+    $stmt->execute(['email' => $email]);
+    return $stmt->fetch(PDO::FETCH_ASSOC);
 }
- 
+  // Função para encontrar usuário pelo ID
+   public static function find($id){
+   $conn = Database::getConnection();
+   $stmt = $conn->prepare("SELECT * FROM usuarios WHERE id = :id");
+   $stmt->execute(['id' => $id]);
+   return $stmt->fetch(PDO::FETCH_ASSOC);
+   }
+   //Função para ceiar um novo usuário no banco dados
+   public static function create($data){
+   $conn = Database::getConnection();
+   $stmt = $conn->prepare("INSERT INTO usuarios (nome, email, senha, perfil) VALUES (:nome, :email, :senha, :perfil)");
+   $stmt->execute($data);
+
+   }
+}
 ?>
